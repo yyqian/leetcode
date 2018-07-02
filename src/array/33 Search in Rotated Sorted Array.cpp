d85633c93e0bbed8ee319faad9258e840745de46
Yinyin Qian <yyqian@live.com>
2018-07-02 18:21:32 +0800

    sync

--- src/array/33 Search in Rotated Sorted Array.cpp
+++ src/array/33 Search in Rotated Sorted Array.cpp
@@ -24,5 +24,34 @@ Output: -1
 using namespace std;
 class Solution {
  public:
-  int search(vector<int> &nums, int target) {}
+  int search(vector<int> &nums, int target) {
+    return helper(nums, target, 0, nums.size() - 1);
+  }
+
+ private:
+  int helper(vector<int> &nums, int target, int start, int end) {
+    if (nums[start] < nums[end]) {
+      if (target < nums[start] || nums[end] < target) {
+        return -1;
+      }
+      while (start <= end) {
+        int mid = start + (end - start) / 2;
+        if (nums[mid] > target) {
+          end = mid - 1;
+        } else if (nums[mid] < target) {
+          start = mid + 1;
+        } else {
+          return mid;
+        }
+      }
+      return -1;
+    } else {
+      int mid = start + (end - start) / 2;
+      if (nums[mid] > nums[start]) {
+        // 顺序
+      } else {
+        // 绕了
+      }
+    }
+  }
 };
