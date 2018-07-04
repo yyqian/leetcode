45ba5f49d7a630061a91d90e21ddd69891fc5726
Yinyin Qian <yyqian@live.com>
2018-07-04 09:55:43 +0800

    auto-sync

--- src/number/43 Multiply Strings.cpp
+++ src/number/43 Multiply Strings.cpp
@@ -23,36 +23,24 @@ using namespace std;
 class Solution {
  public:
   string multiply(string num1, string num2) {
-    reverse(num1);
-    reverse(num2);
-    string result(num1.size() + num2.size() + 1, '0');
-    for (int i = 0; i < num1.size(); ++i) {
+    string result(num1.size() + num2.size(), '0');
+    for (int i = num1.size() - 1; i >= 0; --i) {
       int carry = 0;
-      for (int j = 0; j < num2.size(); ++j) {
-        int tmp = (num1[i] - '0') * (num2[j] - '0') + result[i + j] + carry;
-        result[i + j] = tmp % 10;
+      for (int j = num2.size() - 1; j >= 0; --j) {
+        int tmp = (num1[i] - '0') * (num2[j] - '0') +
+                  (result[i + j + 1] - '0') + carry;
+        result[i + j + 1] = tmp % 10 + '0';
         carry = tmp / 10;
       }
+      result[i] += carry;
     }
-    int i = result.size() - 1;
-    for (; i >= 0; --i) {
+    int i = 0;
+    for (; i < result.size(); ++i) {
       if (result[i] != '0') {
         break;
       }
     }
-    result = result.substr(0, i);
-    reverse(result);
-    return result;
-  }
-
- private:
-  void reverse(string &num) {
-    int i = 0;
-    int j = num.size() - 1;
-    while (i < j) {
-      char tmp = num[i];
-      num[i] = num[j];
-      num[j] = tmp;
-    }
+    result = result.substr(i);
+    return result.empty() ? "0" : result;
   }
 };
