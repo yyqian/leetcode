8a5056455a9cec7eedbdb0c149a558709594fd63
Yinyin Qian <yyqian@live.com>
2018-08-19 23:36:27 +0800

    auto-sync

--- src/137. Single Number II.cpp
+++ src/137. Single Number II.cpp
@@ -1,9 +1,11 @@
 /*
-Given a non-empty array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
+Given a non-empty array of integers, every element appears three times except
+for one, which appears exactly once. Find that single one.
 
 Note:
 
-Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+Your algorithm should have a linear runtime complexity. Could you implement it
+without using extra memory?
 
 Example 1:
 
@@ -13,11 +15,17 @@ Example 2:
 
 Input: [0,1,0,1,0,1,99]
 Output: 99
+
+
+1 ? 1 ? 1 = 0
+0 ? 0 ? 0 = 0
+1 ? 0 = 1
+0 ? 0 = 0
+
+
 */
 
 class Solution {
-public:
-    int singleNumber(vector<int>& nums) {
-
-    }
+ public:
+  int singleNumber(vector<int>& nums) {}
 };
